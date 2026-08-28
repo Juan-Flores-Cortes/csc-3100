e12c6df5d70c99daec1d4a232dd0a6fc395e42f6
Juan-Flores-Cortes <jflor265@calpoly.edu>
2026-08-28 15:57:51 -0700

    Made the change In app.jsx clear

--- Packages/react-frontend/src/App.jsx
+++ Packages/react-frontend/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
         <div>
           <h1>Get started</h1>
           <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+            Edit <code>src/App.jsx</code>  I changed this section of the  text for the class <code>HMR</code>
           </p>
         </div>
         <button
