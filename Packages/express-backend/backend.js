05f3dab96186fe0373ad8d95132a00b09dd76f85
Juan-Flores-Cortes <jflor265@calpoly.edu>
2026-09-16 15:13:59 -0700

    added id

--- Packages/express-backend/backend.js
+++ Packages/express-backend/backend.js
@@ -4,6 +4,10 @@ import express from "express";
 const app = express();
 const port = 8000;
 
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+
 const findUserByName = (name)=> {
   return users ["users_list"].filter((user) => user ["name"] ===name);
 }
@@ -54,6 +58,16 @@ app.post("/users", (req, res) => {
   const userToAdd = req.body;
   addUser(userToAdd);
   res.send();
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id  
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
 });
 
 app.get("/users", (req, res) => {
