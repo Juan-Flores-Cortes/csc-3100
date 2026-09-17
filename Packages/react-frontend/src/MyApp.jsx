ab4888ff19d3321d7261c21c3ce1baae1b4ff4c3
Juan-Flores-Cortes <jflor265@calpoly.edu>
2026-09-16 18:49:41 -0700

    Stage 1 Finished

--- Packages/react-frontend/src/MyApp.jsx
+++ Packages/react-frontend/src/MyApp.jsx
@@ -1,5 +1,5 @@
 // src/MyApp.jsx
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
@@ -9,7 +9,22 @@ function MyApp() {
   function updateList(person) {
   setCharacters([...characters, person]);
  }
-  function removeOneCharacter(index) {
+ 
+ function fetchUsers() {
+  const promise = fetch("http://localhost:8000/users");
+  return promise;
+}
+
+useEffect(() => {
+  fetchUsers()
+    .then((res) => res.json())
+    .then((json) => setCharacters(json["users_list"]))
+    .catch((error) => {
+      console.log(error);
+    });
+}, []);
+
+function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
       return i !== index;
     });
